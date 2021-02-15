5bd24da3d2610e831973999412f60a4e88b3bc1e
Mikhail Zanka <43044718+raznem@users.noreply.github.com>
2021-02-15 08:42:22 +0100

    SAC task

--- rltoolkit/algorithms/sac/sac.py
+++ rltoolkit/algorithms/sac/sac.py
@@ -13,6 +13,15 @@ logger = logging.getLogger(__name__)
 
 
 class SAC(DDPG):
+    ###############################################################
+    # This time your task is to implement essential parts of the
+    # SAC. Same as before, this SAC class is almost ready, the
+    # only part left for you is to finish the update implementation
+    # Also note that this time our actor is stochastic, so forward
+    # is much more complicated than in case of DDPG. You can check
+    # the details of implementatation in the models file:
+    #   rltoolkit/algorithms/sac/models.py
+    ###############################################################
     def __init__(
         self,
         alpha_lr: float = config.ALPHA_LR,
@@ -147,7 +156,28 @@ class SAC(DDPG):
         Returns:
             torch.Tensor: Q-function targets for the batch
         """
-        pass
+        ###############################################################
+        # Implement function for calculating target or your Critic from
+        # the (r, d, s') tuple. See line 12 in the:
+        #   https://spinningup.openai.com/en/latest/algorithms/sac.html
+        #
+        # Hint: This step is very similar to the Critic target from DDPG,
+        # so make sure you have correct gradient flow.
+        # Here and late refer to:
+        #   self.actor_targ: target actor
+        #   self._critic_1: the first critic
+        #   self._critic_2: the second critic
+        #   self._critic_1_targ: target network of the first critic
+        #   self._critic_2_targ: target network of the second critic
+        #   self._actor: actor which return tuple (action, action logprob)
+        #   self.gamma: gamma discount factor
+        #   self.alpha: entropy temperature
+        ###############################################################
+        # Your code is here #
+        qfunc_target =
+
+        # End of your code #
+        return qfunc_target
 
     def compute_pi_loss(
         self,
@@ -165,11 +195,32 @@ class SAC(DDPG):
         Returns:
             torch.Tensor: policy loss
         """
-        pass
+        ###############################################################
+        # Compute policy loss. See line 14 in the pseudocode from:
+        #   https://spinningup.openai.com/en/latest/algorithms/sac.html
+        ###############################################################
+        # Your code is here #
+        loss = 
+        # End of your code #
+        return loss
 
     def update_target_q(self):
         """Update target networks with Polyak averaging"""
-        pass
+        ###############################################################
+        # Same Polyak averaging as in DDPG
+        ###############################################################
+        rho = 1 - self.tau
+        with torch.no_grad():
+            # Polyak averaging:
+            critics_params = chain(
+                self._critic_1.parameters(), self._critic_2.parameters()
+            )
+            targets_params = chain(
+                self.critic_1_targ.parameters(), self.critic_2_targ.parameters()
+            )
+            for q_params, targ_params in zip(critics_params, targets_params):
+                targ_params.data.mul_(rho)
+                targ_params.data.add_((1 - rho) * q_params.data)
 
     def compute_alpha_loss(self, sampled_logprob: torch.Tensor):
         """Compute loss for temperature update
@@ -181,7 +232,14 @@ class SAC(DDPG):
         Returns:
             torch.Tensor: loss for temperature (alpha)
         """
-        pass
+        ###############################################################
+        # Loss for the learnable alpha from the second version of SAC
+        # If you are interested in details see part 5 in the second SAC
+        # paper: https://arxiv.org/abs/1812.05905
+        ###############################################################
+        sampled_logprob = sampled_logprob.detach()
+        alpha_loss = self.log_alpha.exp() * (-sampled_logprob - self.target_entropy)
+        return alpha_loss.mean()
 
     def update(
         self,
@@ -200,7 +258,65 @@ class SAC(DDPG):
             reward (torch.Tensor): rewards tensor
             done (torch.Tensor): dones tensor
         """
-        pass
+        ###############################################################
+        # After implementing particalar parts of the SAC update we
+        # will join them similarly as we joined in DDPG task.
+        # At the beggining implement q-function update.
+        # Refer to the line 13 from OpenAI pseudocode:
+        #   https://spinningup.openai.com/en/latest/algorithms/sac.html
+        # Hint: comparing to the DDPG you have additional critic
+        ###############################################################
+        # Your code is here #
+        loss_q1 = 
+        loss_q2 = 
+        # End of your code #
+
+        self.loss["critic_1"] = loss_q1.item()
+        self.loss["critic_2"] = loss_q2.item()
+
+        self.critic_1_optimizer.zero_grad()
+        loss_q1.backward()
+        self.critic_1_optimizer.step()
+
+        self.critic_2_optimizer.zero_grad()
+        loss_q2.backward()
+        self.critic_2_optimizer.step()
+
+        ###############################################################
+        # Update of the policy network.
+        # You need to finish line 14 from the SAC pseudocode:
+        #   https://spinningup.openai.com/en/latest/algorithms/sac.html
+        # Hint: your actor is stochastic
+        ###############################################################
+        self._critic_1.eval()
+        self._critic_2.eval()
+        # Your code is here #
+        loss = 
+        # End of your code #
+        self.loss["actor"] = loss.item()
+
+        self.actor_optimizer.zero_grad()
+        loss.backward()
+        self.actor_optimizer.step()
+
+        ###############################################################
+        # Update of the target networks
+        ###############################################################
+        self.update_target_q()
+
+        self._critic_1.train()
+        self._critic_2.train()
+
+        ###############################################################
+        # Update of the entropy temperature
+        ###############################################################
+        assert 'sampled_logprob' in locals(), "define sampled_logprob"
+        alpha_loss = self.compute_alpha_loss(sampled_logprob)
+
+        self.alpha_opt.zero_grad()
+        alpha_loss.backward()
+        self.alpha_opt.step()
+        self.alpha = self.log_alpha.exp().item()
 
     def add_tensorboard_logs(self, *args, **kwargs):
         super().add_tensorboard_logs(*args, **kwargs)
@@ -236,21 +352,12 @@ class SAC(DDPG):
 
 
 if __name__ == "__main__":
-    with torch.cuda.device(1):
-        model = SAC(
-            env_name="HalfCheetah-v2",
-            iterations=200,
-            gamma=0.99,
-            batch_size=1000,
-            stats_freq=5,
-            test_episodes=2,
-            update_batch_size=100,
-            update_freq=50,
-            grad_steps=50,
-            # random_frames=10000,
-            use_gpu=True,
-            obs_norm=False,
-            tensorboard_dir="logs_norm",
-            tensorboard_comment="",
-        )
-        model.train()
+    model = SAC(
+        env_name="Pendulum-v0",
+        iterations=100,
+        gamma=0.99,
+        batch_size=50,
+        stats_freq=5,
+        test_episodes=2,
+    )
+    model.train()
